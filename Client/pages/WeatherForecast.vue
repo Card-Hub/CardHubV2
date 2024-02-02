837331c733cb29ae824f5eb60be7839aa1380dc5
mozqueda-alejandro <admozqueda@nevada.unr.edu>
2024-02-01 17:36:30 -0800

    refactor: edit WebApi cloud deployment config

---
+++ Client/pages/WeatherForecast.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import {useApi} from "@/composables/useApi";
+
+type Forecasts = Array<{
+  date: string
+  temperatureC: number
+  temperatureF: number
+  summary: string
+}>
+
+// Example usage of data fetching from the ASP.NET Core backend
+// Use the useApi composable for this
+const {data, pending, error} = await useApi<Forecasts>("/weatherforecast");
+
+</script>
+
+<template>
+  <div>
+    <h1>Weather forecast</h1>
+    <p>This component demonstrates fetching data from the server.</p>
+    <div v-if="error != null">The backend is not running or there is an error in the API call.</div>
+    <div v-else-if="pending">Loading data from the API...</div>
+    <div v-else>
+      <table>
+        <thead>
+        <tr>
+          <th>Date</th>
+          <th>Temp. (C)</th>
+          <th>Temp. (F)</th>
+          <th>Summary</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="forecast in data as Forecasts" :key="forecast.date">
+          <td>{{ forecast.date }}</td>
+          <td>{{ forecast.temperatureC }}</td>
+          <td>{{ forecast.temperatureF }}</td>
+          <td>{{ forecast.summary }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>

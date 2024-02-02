ccdadd67781adb1bf935f2b9685149f4873e9e64
dionisio-rubi <rdionisio@nevada.unr.edu>
2024-02-01 20:46:53 -0800

    cards correctly formatted

--- Client/components/PlayingCard.vue
+++ Client/components/PlayingCard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { defineProps } from 'vue';
+
   // props are passed from the parent component
   type Card = {
     id: number
@@ -29,34 +30,17 @@ import { defineProps } from 'vue';
     }
   };
 
-  // const getSuitIcon = async (suit: string) => {
-  //   try{
-  //     switch (suit) {
-  //       case 'Hearts':
-  //         return (await import('../assets/icons/heart.svg')).default;
-  //       case 'Diamonds':
-  //         return (await import(../assets/icons/diamonds.svg')).default;
-  //       case 'Clubs':
-  //         return (await import('../assets/icons/clubs.svg')).default;
-  //       case 'Spades':
-  //         return (await import('../assets/icons/spade.svg')).default;
-  //       default:
-  //         return '';
-  //     }
-  //   } catch (error) {
-  //     console.error(`Error loading icon for suit ${suit}:`, error);
-  //     return '';
-  //   }
-  //  
-  // };
+// https://stackoverflow.com/questions/56624817/passing-and-binding-img-src-from-props-in-vue-js
+const getSuitIcon = (suit: string) => {
+  console.log(`../assets/icons/${suit.toLowerCase()}.svg`);
+  return new URL(`../assets/icons/${suit.toLowerCase()}.svg`, import.meta.url);
+};
 
-const getSuitIcon = async (suit: string) => {
-  try {
-    const iconModule = await import(`@/assets/icons/${suit.toLowerCase()}.svg`);
-    return iconModule.default;
-  } catch (error) {
-    console.error(`Error loading icon for suit ${suit}:`, error);
-    return '';
+const getSuitColor = (suit: string) => {
+  if (suit === 'Hearts' || suit === 'Diamonds') {
+    return 'red';
+  } else {
+    return 'black';
   }
 };
 </script>
@@ -65,15 +49,19 @@ const getSuitIcon = async (suit: string) => {
   <div class="flex flex-wrap justify-center items-center">
     <div
         v-for="card in playingCards"
+        :key="card.id"
         class="relative w-20 h-32 m-2 bg-white rounded-md shadow-md p-2"
+        style="box-shadow: 6px -6px 3px rgba(200, 200, 200, 0.4);"
     >
       <img
           :src="getSuitIcon(card.suit)"
           alt="suit icon"
-          class="absolute bottom-2 right-2 w-10 h-10"
+          class="absolute bottom-2 right-2 w-14 h-14"
       />
-      <div class="absolute top-2 left-2 text-Slg font-bold text-black">{{ getDisplayValue(card.value) }}</div>
-<!--      <div class="absolute bottom-2 right-2 text-lg font-bold text-black">{{ card.value }}</div>-->
+      <div
+          class="absolute top-2 left-2 text-4xl font-bold"
+          :style="{ color: getSuitColor(card.suit) }" 
+          >{{ getDisplayValue(card.value) }}</div>
     </div>
   </div>
 </template>

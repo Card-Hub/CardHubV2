3aa7c647cf36d0a42e5798fb9ac538fc59c4aa58
dionisio-rubi <rdionisio@nevada.unr.edu>
2024-03-01 21:29:30 -0800

    test reverting back to original deployable commits

---
+++ Client/plugins/api.ts
@@ -0,0 +1,16 @@
+﻿import { ofetch } from 'ofetch'
+
+// Allows client-side fetching configured with a global header URL
+// https://stackoverflow.com/a/75605263/18790415
+export default defineNuxtPlugin(() => {
+    const runtimeConfig = useRuntimeConfig();
+    const instance = ofetch.create({
+        baseURL: runtimeConfig.public.baseURL
+    })
+
+    return {
+        provide: {
+            api: instance
+        }
+    }
+})

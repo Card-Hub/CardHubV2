0e1b05082a74af505508446d4ecc8f3d6223c0d1
Lyssie Brown <abrown@nevada.unr.edu>
2024-03-06 19:53:27 -0800

    minor: stubbed out more functionality

--- WebApi/GameLogic/UnoGame.cs
+++ WebApi/GameLogic/UnoGame.cs
@@ -8,9 +8,12 @@ public class UnoGame : IBaseGame {
   private List<String> playerList = new List<String>();
   private List<UnoCard> deck = new List<UnoCard>();
   private int directionInt = 1;
+  private int CurrentPlayerIndex = 0;
+  //private Dictionary<String, List<UnoCard>> playerHands;
   public UnoGame() {
     playerList = [];
     this.InitDeck();
+    //this.SetUpHands();
   }
   public void InitDeck() {
     // Add cards to deck
@@ -41,7 +44,10 @@ public class UnoGame : IBaseGame {
   }
 
   public void StartGame() {
-    throw new NotImplementedException();
+    // Randomize list of players
+    // Player whose turn it is, is first player
+    Console.WriteLine("Hi");
+    //throw new NotImplementedException();
   }
   public List<String> GetPlayerList() {
     return playerList;
@@ -88,15 +94,44 @@ public class UnoGame : IBaseGame {
     Random rng = new Random();
     deck = deck.OrderBy(_ => rng.Next()).ToList();
   }
+  public void shufflePlayers() {
+    Random rng = new Random();
+    playerList = playerList.OrderBy(_ => rng.Next()).ToList();
+  }
   public void EndGame() {
     throw new NotImplementedException();
   }
-  public UnoCard drawCard() {
+  public UnoCard popTopCard() {
     UnoCard card = deck.ElementAt(0);
     deck.RemoveAt(0);
     return card;
   }
-  public bool tryPlayCard(UnoCard card) {
+  //public bool drawCard(String playerName) {
+  //  if (playerName == GetCurrentPlayer()) {
+  //    player
+  //  }
+  //}
+  public bool TryPlayCard(UnoCard card) {
     return true;
   }
+  public bool ReverseDirection() {
+    directionInt /= -1; // -1 -> 1, or 1 -> -1
+    return true;
+  }
+  public UnoCard GetTopCard() {
+    return deck.ElementAt(0);
+  }
+  //public UnoCard Assign7CardsToPlayer(playerName) {
+  //  for (int j = 0; j < 7; j++) {
+  //    currentPlayer = playerName;
+
+  //  }
+  //}
+  //public 
+  //public void SetUpHands() {
+  //  for (int i = 0; i < playerList.Count; i++) {
+  //    Console.WriteLine("Assigning card to " + playerList.ElementAt(i));
+
+  //  }
+  //}
 }

6d06393caf4433bae38b6c02e75ecf8a6a6f4aec
Lyssie Brown <abrown@nevada.unr.edu>
2024-03-07 02:43:48 -0800

    feat: uno logic (mostly done)

--- WebApi/GameLogic/UnoGame.cs
+++ WebApi/GameLogic/UnoGame.cs
@@ -7,10 +7,14 @@ namespace WebApi.GameLogic;
 public class UnoGame : IBaseGame {
   // constructor
   private List<String> playerList = new List<String>();
-  private List<UnoCard> deck = new List<UnoCard>();
+  private List<UnoCard> Deck = new List<UnoCard>();
+  private List<UnoCard> DiscardPile = new List<UnoCard>();
   private int DirectionInt = 1;
   private int CurrentPlayerIndex = 0;
+  private bool someoneHasWon = false;
   private Dictionary<String, List<UnoCard>> playerHands = new Dictionary<string, List<UnoCard>>();
+  private string WildColor = "";
+  private string PlayerWhoNeedsToPickWildColor = "";
   public UnoGame() {
     playerList = [];
     this.InitDeck();
@@ -19,7 +23,7 @@ public class UnoGame : IBaseGame {
   /// Initialize the deck. Resets it to the 100 something cards.
   /// </summary>
   public void InitDeck() {
-    deck.Clear();
+    Deck.Clear();
     // Add cards to deck
     String[] colors = ["red", "blue", "yellow", "green"];
     String[] coloredValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Draw Two", "Draw Two", "Reverse", "Reverse", "Skip", "Skip", "Skip All", "Skip All"];
@@ -32,7 +36,7 @@ public class UnoGame : IBaseGame {
         newCard.id = cardId;
         newCard.value = value;
         newCard.color = color;
-        deck.Add(newCard);
+        Deck.Add(newCard);
         cardId++;
       }
     }
@@ -42,7 +46,7 @@ public class UnoGame : IBaseGame {
         newCard.id = cardId;
         newCard.value = wildCardValue;
         newCard.color = "black";
-        deck.Add(newCard);
+        Deck.Add(newCard);
         cardId++;
       }
     }
@@ -58,14 +62,20 @@ public class UnoGame : IBaseGame {
       playerHands[playerName] = new List<UnoCard>();
       Assign7CardsToPlayer(playerName);
     }
-    //throw new NotImplementedException();
+    // put first card on discard pile until there's no wild card
+    bool wildOnTop = true;
+    while (wildOnTop) {
+      DiscardPile.Insert(0, PopTopCard());
+      wildOnTop = DiscardPile.ElementAt(0).color == "black";
+    }
+    Console.WriteLine("Card on top of discard pile is " + GetUnoCardString(DiscardPile.ElementAt(0)));
   }
   public List<String> GetPlayerList() {
     return playerList;
     throw new NotImplementedException();
   }
    public List<UnoCard> GetDeck() {
-    return deck;
+    return Deck;
     throw new NotImplementedException();
   }
 
@@ -114,7 +124,7 @@ public class UnoGame : IBaseGame {
   }
   public void ShuffleDeck() {
     Random rng = new Random();
-    deck = deck.OrderBy(_ => rng.Next()).ToList();
+    Deck = Deck.OrderBy(_ => rng.Next()).ToList();
   }
   public void ShufflePlayers() {
     Random rng = new Random();
@@ -124,8 +134,8 @@ public class UnoGame : IBaseGame {
     throw new NotImplementedException();
   }
   public UnoCard PopTopCard() {
-    UnoCard card = deck.ElementAt(0);
-    deck.RemoveAt(0);
+    UnoCard card = Deck.ElementAt(0);
+    Deck.RemoveAt(0);
     return card;
   }
   public bool DrawCard(String playerName) {
@@ -143,7 +153,7 @@ public class UnoGame : IBaseGame {
     return true;
   }
   public UnoCard GetTopCard() {
-    return deck.ElementAt(0);
+    return Deck.ElementAt(0);
   }
   public void Assign7CardsToPlayer(string playerName) {
     CurrentPlayerIndex = playerList.IndexOf(playerName);
@@ -163,7 +173,14 @@ public class UnoGame : IBaseGame {
   public string GetCurrentPlayer() {
     return playerList[CurrentPlayerIndex];
   }
+  public bool PlayerHasPlayableCard(string playerName) {
+    foreach (UnoCard card in GetPlayerHand(playerName)) {
+      if (CardCanBePlayed(card)) {return true;}
+    }
+    return false;
+  }
   public void NextTurn() {
+    Console.WriteLine("Next turn!");
     CurrentPlayerIndex += DirectionInt;
     if (CurrentPlayerIndex >= playerList.Count) {
       CurrentPlayerIndex = 0;
@@ -171,5 +188,118 @@ public class UnoGame : IBaseGame {
     if (CurrentPlayerIndex < 0) {
       CurrentPlayerIndex = playerList.Count - 1;
     }
+  }
+  public List<UnoCard> GetDiscardPile() {
+    return DiscardPile;
+  }
+  public UnoCard GetTopCardInDiscardPile() {
+    return DiscardPile.ElementAt(0);
+  }
+  public void updateWildColor(string color) {
+    WildColor = color;
+    PlayerWhoNeedsToPickWildColor = "";
+  }
+  public bool CardCanBePlayed(UnoCard cardToPlay) {
+    bool cardCanBePlayed = false;
+    bool cardMatchesValue = GetTopCardInDiscardPile().value == cardToPlay.value;
+    bool cardMatchesColor = GetTopCardInDiscardPile().color == cardToPlay.color;
+    // handle if there's a wild on the top card
+    bool cardMatchesWildColor = false;
+    if (GetTopCardInDiscardPile().value == "Wild" || GetTopCardInDiscardPile().value == "Wild Draw Four") {
+      if (cardToPlay.color == WildColor) {
+        cardMatchesWildColor = true;
+      }
+    }
+    // handle if cardToPlay is a wild card itself
+    bool cardToPlayIsWild = cardToPlay.value == "Wild" || cardToPlay.value == "Wild Draw 4";
+
+    cardCanBePlayed = cardMatchesValue || cardMatchesColor || cardMatchesWildColor || cardToPlayIsWild;
+    return cardCanBePlayed;
+  }
+  public bool PlayCard(string playerName, UnoCard card) {
+    string[] numericalVals = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    string[] actionCards = {"Skip", "Draw Two", "Reverse"};
+    string[] wildCards = {"Wild", "Wild Draw Four"};
+
+    if (playerName == GetCurrentPlayer() || CardCanBePlayed(card)) {
+      // check what type of card it is
+      bool cardIsNumerical = Array.IndexOf(numericalVals, card.value) != -1;
+      bool cardIsAction = Array.IndexOf(actionCards, card.value) != -1;
+      bool cardIsWild = Array.IndexOf(wildCards, card.value) != -1;
+
+
+      // put card on top of the discard pile
+      DiscardPile.Insert(0, card);
+      // remove card from player hand
+      playerHands[playerName].Remove(card);
+      Console.WriteLine(playerName + " successfully played a card: " + GetUnoCardString(card));
+      // handle card effects
+      if (cardIsNumerical) { // nothing more to do
+      }
+      else if (cardIsAction) {
+        if (card.value == "Skip") {
+          NextTurn();
+        }
+        else if (card.value == "Reverse") {
+          ReverseDirection();
+        }
+        else if (card.value == "Draw 2") {
+          NextTurn();
+          DrawCard(GetCurrentPlayer());
+          DrawCard(GetCurrentPlayer());
+        }
+        else {
+          Console.WriteLine("Played unknown action card.");
+        }
+      }
+      else if (cardIsWild) {
+          SetPlayerNeedsToPickWildColor(GetCurrentPlayer());
+          Console.WriteLine(playerName + " needs to pick a wild color now. It is still their turn.");
+          // handle player needs to pick wild elsewhere
+      }
+      foreach (string name in GetPlayerList()) {
+        if (GetPlayerHand(name).Count == 0) {
+          someoneHasWon = true;
+          Console.WriteLine(name + " has won ~");
+      }
+      }
+      if (!cardIsWild) {
+        NextTurn();
+      }
+      return true;
+    }
+    return false;
+  }
+  public void SetPlayerNeedsToPickWildColor(string playerName) {
+    PlayerWhoNeedsToPickWildColor = playerName;
+  }
+
+  public bool PlayerNeedsToPickWildColor(string playerName) {
+    return playerName == PlayerWhoNeedsToPickWildColor;
+  }
+  public bool SetWildColor(string playerName, string color) {
+    if (playerName == GetCurrentPlayer()) {
+      if (playerName == PlayerWhoNeedsToPickWildColor) {
+        WildColor = color;
+        Console.WriteLine(playerName + " set the wild color to be " + color);
+        PlayerWhoNeedsToPickWildColor = "";
+        NextTurn();
+        return true;
+      }
+      else {
+        Console.WriteLine(playerName + " cannot set the wild color, it's not their job!");
+        return false;
+      }
+    }
+    else {
+      Console.WriteLine("ERROR: " + playerName + " cannot set the wild color, it's not their turn!");
+      return false;
+    }
+  }
+  public void ResetWildColor() {
+    WildColor = "";
+  }
+  public bool IsOngoing() {
+    return !someoneHasWon;
   }
 }

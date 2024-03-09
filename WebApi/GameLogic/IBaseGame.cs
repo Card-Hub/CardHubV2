e2c74274c0eddad18e71a0a1b99c7c2b159acf86
Lyssie Brown <abrown@nevada.unr.edu>
2024-03-09 13:58:38 -0800

    minor change to interface

--- WebApi/GameLogic/IBaseGame.cs
+++ WebApi/GameLogic/IBaseGame.cs
@@ -1,9 +1,13 @@
-namespace WebApi.GameLogic;
+﻿namespace WebApi.GameLogic;
 
 public interface IBaseGame {
   void StartGame();
   List<String> GetPlayerList();
   Boolean AddPlayer(String playerName);
   Boolean RemovePlayer( String playerName);
+
+  //List<iCard> GetPlayerHand(string playerName);
+
+
   //void EndGame();
 }

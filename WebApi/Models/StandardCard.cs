b64ad9334413b99d8de2bb254ed91a11dfa38665
Lyssie Brown <abrown@nevada.unr.edu>
2024-03-15 02:09:22 -0700

    added iGame, iCard, refactored UnoCard to be 'backwards compatible, added unit testing solution with xUnit

--- WebApi/Models/StandardCard.cs
+++ WebApi/Models/StandardCard.cs
@@ -2,7 +2,27 @@
 
 public class StandardCard
 {
-    public int id { get; set; }
-    public string suit { get; set; }
-    public string value { get; set; }
+    public StandardCard() {
+      this.Id = 0;
+      this.Suit = "";
+      this.Value = "";
+    }
+    public StandardCard(int id, string suit, string value) : this() {
+      this.Id = id;
+      this.Suit = suit;
+      this.Value = value;
+    }
+    public int Id { get; set; }
+    public string Suit { get; set; }
+    public string Value { get; set; }
+
+    // manually doing json is a red flag but oh well lmao
+    public string GetValues()
+    {
+      string innerJson = "";
+      innerJson += "id: " + this.Id + ",";
+      innerJson += "suit: " + this.Suit + ",";
+      innerJson += "value: " + this.Value;
+      return $"{{{innerJson}}}";
+    }
 }

2312544c21bd4f5e583e9fe409df22398f91d69b
Lyssie Brown <abrown@nevada.unr.edu>
2024-04-23 12:17:54 -0700

    small modifications to lobby, chat, cah flavor text

--- Client/components/Chat.vue
+++ Client/components/Chat.vue
@@ -5,8 +5,9 @@ import { storeToRefs } from "pinia";
 
 const store = useWebSocketStore();
 const { messages} = useWebSocketStore();
-const { lobbyUsers } = storeToRefs(store);
+// const { lobbyUsers } = storeToRefs(store);
 const { sendMessage } = store;
+ref
 const newMessage1 = ref('');//find a way to get rid fo this
 const handleSendMessage = () => {
     console.log("inside handlemessage", newMessage1.value);
@@ -21,6 +22,7 @@ const getIcon = (avatar: string) => {
 </script>
 
 <template>
+  <div class="chat-container">
     <h2 class="title text-center">Chat</h2>
     <div v-for="(m, index) in messages" :key="index">
         <div v-for="lobbyUser in lobbyUsers as LobbyUser[]">
@@ -31,9 +33,10 @@ const getIcon = (avatar: string) => {
         <div class="bg-primary"> {{ m.message }}</div>
     </div>
     <div>
-      <input class="p-inputtext1" type="text" v-model="newMessage1" placeholder="Type your message..." />
+      <input class="input-yeee p-inputtext1" type="text" v-model="newMessage1" placeholder="Type your message..." />
       <button class="send-button" @click="handleSendMessage" :disabled="(newMessage1=='')">Send</button>
     </div>
+  </div>
 </template>
 
 <style scoped>
@@ -43,8 +46,21 @@ const getIcon = (avatar: string) => {
 
 }
 
+.chat-container {
+  background-color: #3d0b0b;
+  width: 20%;
+  padding: 10px;
+  border-radius: 15px;
+}
+
+.input-yeee {
+  width: 80%;
+  height: 30px;
+  color:
+}
+
 .lobby-player-icon-img {
-    width: 3em;
+    width: 2em;
     border-radius: 50%; /* Ensure the player icon is circular */
   overflow: hidden;
   }
